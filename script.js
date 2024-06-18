ced33f32f0617d3987e7a11574b2a7a7df8c5552
Anany717 <134945045+Anany717@users.noreply.github.com>
2024-06-18 18:21:38 -0400

    Update script.js(Adding ColorPicker)

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
 let colorSelected; 
 
-//Adds a row
 function addR() {
     let grid = document.getElementById("grid");
     let rows = document.getElementsByTagName("tr");
 
-    //if grid is empty need to create row
     if (rows.length === 0) {
         let row = document.createElement("tr");
         let col = document.createElement("td");
-        col.onclick = function(){
+        col.onclick = function() {
             this.style.backgroundColor = colorSelected;
         };
 
         row.appendChild(col);
         grid.appendChild(row);
-    //otherwise append a new row with the current
-    //amount of columns
     } else {
         let numCols = rows[0].childElementCount;
         let row = document.createElement("tr");
-        for (let i = 0; i < numCols; i++){
+        for (let i = 0; i < numCols; i++) {
             let col = document.createElement("td");
-            col.onclick = function(){
-              this.style.backgroundColor = colorSelected;
+            col.onclick = function() {
+                this.style.backgroundColor = colorSelected;
             };
             row.appendChild(col);
         }
         grid.appendChild(row);
     }
 }
-//Adds a column
+
 function addC() {
     let grid = document.getElementById("grid");
     let rows = document.getElementsByTagName("tr");
@@ -38,42 +34,39 @@ function addC() {
     if (rows.length === 0) {
         let row = document.createElement("tr");
         let col = document.createElement("td");
-        col.onclick = function (){
+        col.onclick = function () {
             this.style.backgroundColor = colorSelected;
         };
         row.appendChild(col);
         grid.appendChild(row);
-
     } else {
-        for (let i = 0; i < rows.length; i++){
+        for (let i = 0; i < rows.length; i++) {
             let col = document.createElement("td");
-            col.onclick = function(){
-              this.style.backgroundColor = colorSelected;
+            col.onclick = function() {
+                this.style.backgroundColor = colorSelected;
             };
             rows[i].appendChild(col);
         }
     } 
 }
 
-//Removes a row
 function removeR() {
     let grid = document.getElementById("grid");
     let rows = document.getElementsByTagName("tr");
-    if(rows.length === 0){
+    if(rows.length === 0) {
         alert("There is nothing to delete");
         return;
     }
 
     let lastRow = grid.lastElementChild;
     grid.removeChild(lastRow);
-    
 }
-//Remove a column
+
 function removeC() {
     let rows = document.getElementsByTagName("tr");
     let grid = document.getElementById("grid");
 
-    if(rows.length === 0){
+    if(rows.length === 0) {
         alert("There is nothing to delete");
         return;
     }
@@ -83,40 +76,38 @@ function removeC() {
         return;
     } 
 
-    for (let i = 0; i < rows.length; i++){
+    for (let i = 0; i < rows.length; i++) {
         let col = rows[i].lastElementChild; 
         rows[i].removeChild(col);
     }
-
-}
-//sets global variable for selected color
-function selected(){
-    colorSelected = document.getElementById("selectedID").value;
 }
 
-function fill(){
+function selected() {
+    colorSelected = document.getElementById("colorPicker").value;
+}
+
+function fill() {
     let cells = document.getElementsByTagName("td");
 
-    for (let i = 0; i < cells.length; i++){
+    for (let i = 0; i < cells.length; i++) {
         cells[i].style.backgroundColor = colorSelected;
     }
 }
 
-function clearAll(){
+function clearAll() {
     let cells = document.getElementsByTagName("td");
 
-    for (let i = 0; i < cells.length; i++){
+    for (let i = 0; i < cells.length; i++) {
         cells[i].style.backgroundColor = "";
     }
 }
 
-function fillU(){
+function fillU() {
     let cells = document.getElementsByTagName("td");
 
-    for (let i = 0; i < cells.length; i++){
+    for (let i = 0; i < cells.length; i++) {
         if (cells[i].style.backgroundColor === "") {
             cells[i].style.backgroundColor = colorSelected;
         }
     }
 }
-
